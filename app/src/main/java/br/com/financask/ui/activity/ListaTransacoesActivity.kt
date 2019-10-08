e0971b672db7cc176a0e779d22d4953e6639b4a2
Douglas Sousa <douglascamilo@gmail.com>
2019-10-08 17:57:12 -0300

    [Aula 01] - Implementando o próprio Adapter

--- app/src/main/java/br/com/financask/ui/activity/ListaTransacoesActivity.kt
+++ app/src/main/java/br/com/financask/ui/activity/ListaTransacoesActivity.kt
@@ -3,11 +3,13 @@ package br.com.financask.ui.activity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import br.com.financask.R
+import kotlinx.android.synthetic.main.activity_lista_transacoes.*
 
 class ListaTransacoesActivity: AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lista_transacoes)
+
     }
 }
---
+++ app/src/main/java/br/com/financask/ui/adapter/ListaTransacoesAdapter.kt
@@ -0,0 +1,24 @@
+package br.com.financask.ui.adapter
+
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+
+class ListaTransacoesAdapter: BaseAdapter() {
+
+    override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View {
+        
+    }
+
+    override fun getItem(p0: Int): Any {
+
+    }
+
+    override fun getItemId(p0: Int): Long {
+
+    }
+
+    override fun getCount(): Int {
+
+    }
+}
